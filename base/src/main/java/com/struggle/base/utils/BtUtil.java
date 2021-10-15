b08a42317f014028b6ca62b6d263b314b70aa69b
dengjianzhong <330035231@qq.com>
2021-10-15 16:37:03 +0800

    新增蓝牙类型工具类

---
+++ base/src/main/java/com/struggle/base/utils/BtUtil.java
@@ -0,0 +1,142 @@
+package com.struggle.base.utils;
+
+import android.bluetooth.BluetoothClass;
+
+/**
+ * @Author 邓建忠
+ * @CreateTime 2020/5/6 10:31
+ * @Description 筛选蓝牙打印机
+ */
+public class BtUtil {
+    public static final int PROFILE_HEADSET = 0;
+
+    public static final int PROFILE_A2DP = 1;
+
+    public static final int PROFILE_OPP = 2;
+
+    public static final int PROFILE_HID = 3;
+
+    public static final int PROFILE_PANU = 4;
+
+    public static final int PROFILE_NAP = 5;
+
+    public static final int PROFILE_A2DP_SINK = 6;
+
+    /**
+     * 获取蓝牙设备类型
+     *
+     * @param bluetoothClass
+     * @return
+     */
+    public static int getDeviceType(BluetoothClass bluetoothClass) {
+        if (bluetoothClass == null) {//未知设备
+            return 0x10;
+        }
+        switch (bluetoothClass.getMajorDeviceClass()) {
+            case BluetoothClass.Device.Major.COMPUTER://电脑
+                return 0x11;
+            case BluetoothClass.Device.Major.PHONE://手机
+                return 0x12;
+            case BluetoothClass.Device.Major.PERIPHERAL://外围设备
+                return 0x13;
+            case BluetoothClass.Device.Major.IMAGING://打印机
+                return 0x14;
+            default:
+                if (BtUtil.doesClassMatch(bluetoothClass, PROFILE_HEADSET)) {//耳机
+                    return 0x15;
+                } else if (BtUtil.doesClassMatch(bluetoothClass, PROFILE_A2DP)) {//立体声耳机
+                    return 0x16;
+                } else {//未知设备
+                    return 0x17;
+                }
+        }
+    }
+
+    /**
+     * @param bluetoothClass
+     * @param profile
+     * @return
+     */
+    public static boolean doesClassMatch(BluetoothClass bluetoothClass, int profile) {
+        if (profile == PROFILE_A2DP) {
+            if (bluetoothClass.hasService(BluetoothClass.Service.RENDER)) {
+                return true;
+            }
+            // By the A2DP spec, sinks must indicate the RENDER service.
+            // However we found some that do not (Chordette). So lets also
+            // match on some other class bits.
+            switch (bluetoothClass.getDeviceClass()) {
+                case BluetoothClass.Device.AUDIO_VIDEO_HIFI_AUDIO:
+                case BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES:
+                case BluetoothClass.Device.AUDIO_VIDEO_LOUDSPEAKER:
+                case BluetoothClass.Device.AUDIO_VIDEO_CAR_AUDIO:
+                    return true;
+                default:
+                    return false;
+            }
+        } else if (profile == PROFILE_A2DP_SINK) {
+            if (bluetoothClass.hasService(BluetoothClass.Service.CAPTURE)) {
+                return true;
+            }
+            // By the A2DP spec, srcs must indicate the CAPTURE service.
+            // However if some device that do not, we try to
+            // match on some other class bits.
+            switch (bluetoothClass.getDeviceClass()) {
+                case BluetoothClass.Device.AUDIO_VIDEO_HIFI_AUDIO:
+                case BluetoothClass.Device.AUDIO_VIDEO_SET_TOP_BOX:
+                case BluetoothClass.Device.AUDIO_VIDEO_VCR:
+                    return true;
+                default:
+                    return false;
+            }
+        } else if (profile == PROFILE_HEADSET) {
+            // The render service class is required by the spec for HFP, so is a
+            // pretty good signal
+            if (bluetoothClass.hasService(BluetoothClass.Service.RENDER)) {
+                return true;
+            }
+            // Just in case they forgot the render service class
+            switch (bluetoothClass.getDeviceClass()) {
+                case BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE:
+                case BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET:
+                case BluetoothClass.Device.AUDIO_VIDEO_CAR_AUDIO:
+                    return true;
+                default:
+                    return false;
+            }
+        } else if (profile == PROFILE_OPP) {
+            if (bluetoothClass.hasService(BluetoothClass.Service.OBJECT_TRANSFER)) {
+                return true;
+            }
+
+            switch (bluetoothClass.getDeviceClass()) {
+                case BluetoothClass.Device.COMPUTER_UNCATEGORIZED:
+                case BluetoothClass.Device.COMPUTER_DESKTOP:
+                case BluetoothClass.Device.COMPUTER_SERVER:
+                case BluetoothClass.Device.COMPUTER_LAPTOP:
+                case BluetoothClass.Device.COMPUTER_HANDHELD_PC_PDA:
+                case BluetoothClass.Device.COMPUTER_PALM_SIZE_PC_PDA:
+                case BluetoothClass.Device.COMPUTER_WEARABLE:
+                case BluetoothClass.Device.PHONE_UNCATEGORIZED:
+                case BluetoothClass.Device.PHONE_CELLULAR:
+                case BluetoothClass.Device.PHONE_CORDLESS:
+                case BluetoothClass.Device.PHONE_SMART:
+                case BluetoothClass.Device.PHONE_MODEM_OR_GATEWAY:
+                case BluetoothClass.Device.PHONE_ISDN:
+                    return true;
+                default:
+                    return false;
+            }
+        } else if (profile == PROFILE_HID) {
+            return (bluetoothClass.getDeviceClass() & BluetoothClass.Device.Major.PERIPHERAL) == BluetoothClass.Device.Major.PERIPHERAL;
+        } else if (profile == PROFILE_PANU || profile == PROFILE_NAP) {
+            // No good way to distinguish between the two, based on class bits.
+            if (bluetoothClass.hasService(BluetoothClass.Service.NETWORKING)) {
+                return true;
+            }
+            return (bluetoothClass.getDeviceClass() & BluetoothClass.Device.Major.NETWORKING) == BluetoothClass.Device.Major.NETWORKING;
+        } else {
+            return false;
+        }
+    }
+}
